dd6b76345a8cf2ae54298ddadf013b554696f8d6
ChenkeZhang <58590794+ChenkeZhang@users.noreply.github.com>
2025-11-21 18:20:08 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,21 @@
+const CACHE_NAME = 'wearwise-v1.1'; // 升级版本号
 self.addEventListener('install', (e) => {
+  self.skipWaiting(); // 强制立刻更新
   e.waitUntil(
-    caches.open('wearwise-store').then((cache) => cache.addAll([
+    caches.open(CACHE_NAME).then((cache) => cache.addAll([
       '/',
       '/index.html',
     ])),
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) return caches.delete(key); // 删除旧缓存
+      }));
+    })
   );
 });
 
